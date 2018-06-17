4468a60e96cbfbde6bdc0589697bc9dde3ef6351
loaner <loaner@bsmjyf5yvair.event.lanets.ca>
2018-06-17 16:19:33 -0400

    backwards and forwards!

--- newfollowing.py
+++ newfollowing.py
@@ -30,6 +30,7 @@ rgo = 1200
 rslow = 1350
 lslow = 1650
 
+
 fardist = 370
 closedist = 320
 
@@ -112,3 +113,52 @@ while True: # big loop
         if straight > 2:
             RPL.servoWrite(motorL,lgo)
             RPL.servoWrite(motorR,rslow)
+
+    while True: # backward
+        RPL.analogRead(fana)
+        RPL.analogRead(back)
+        Fanalog = RPL.analogRead(fana)
+        Banalog = RPL.analogRead(bana)
+        frsensor = RPL.digitalRead(frdig)
+        flsensor = RPL.digitalRead(fldig)
+        brsensor = RPL.digitalRead(brdig)
+        blsensor = RPL.digitalRead(bldig)
+
+        straight = Banalog - Fanalog
+
+
+        # Turns:
+
+        while blsensor == 0: # sharp left in wall, turn left
+            RPL.servoWrite(motorL,rslow)
+            RPL.servoWrite(motorR,rslow)
+
+        while brsensor == 0: # sharp right in wall, turn right
+            RPL.servoWrite(motorL,lslow)
+            RPL.servoWrite(motorR,lslow)
+
+        if brsensor = 0 and blsensor = 0:
+            break
+
+        # Staying straight:
+
+        # calibrating the distance off the wall:
+        if Fanalog <= closedist and Banalog <= closedist:
+            RPL.servoWrite(motorL,lgo)
+            RPL.servoWrite(motorR,rslow)
+
+        if Fanalog >= fardist and Banalog >= fardist:
+            RPL.servoWrite(motorL,lslow)
+            RPL.servoWrite(motorR,rgo)
+
+        #if the robot is parallel to the wall it will move forward
+        if straight > -2 and straight < 2:
+            forward()
+        #if the robot is angled away the wall- turn towards
+        if straight < -2:
+            RPL.servoWrite(motorL,rslow)
+            RPL.servoWrite(motorR,lgo)
+        #if the robot is angeled towards the wall- turn away
+        if straight > 2:
+            RPL.servoWrite(motorL,rgo)
+            RPL.servoWrite(motorR,lslow)
